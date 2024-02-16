6624ac94fec826a6167aa3e1ec1f457f8d773bc3
Sajawal Fareedi <sajawalfareedi448@gmail.com>
2024-02-16 15:27:18 +0500

    fixing mongodb connection error

--- db.js
+++ db.js
@@ -4,15 +4,17 @@ const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://linkedin-hot-leads:ilXR2tTqnF2XDQlW@linkedin-db.tt3ronc.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToDB = async () => {
-    await mongoose.connect(MONGODB_URI, { connectTimeoutMS: 120000, socketTimeoutMS: 150000, dbName: "linkedin-db" })
-        .catch((err) => { console.trace(err) });
+    if (mongoose.connection.readyState !== 1) {
+        await mongoose.connect(MONGODB_URI, { connectTimeoutMS: 120000, socketTimeoutMS: 150000, dbName: "linkedin-db" })
+            .catch((err) => { console.trace(err) });
+        
+        mongoose.connection.readyState === 1 ? console.info("Connected to MongoDB") : await connectToDB();
+    }
 }
 
 const main = async () => {
     try {
         await connectToDB();
-
-        mongoose.connection.readyState === 1 ? console.info("Connected to MongoDB") : await connectToDB();
 
         if (!mongoose.modelNames().includes("Cookie")) {
             const cookieSchema = new mongoose.Schema({
