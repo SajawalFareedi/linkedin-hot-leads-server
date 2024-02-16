389d7b34d8705883ceb8f6c5700b95db78e6ad94
Sajawal Fareedi <sajawalfareedi448@gmail.com>
2024-02-16 11:34:19 +0500

    fixing mongodb connection error

--- db.js
+++ db.js
@@ -2,18 +2,21 @@ const mongoose = require('mongoose');
 
 
 const main = async () => {
-    await mongoose.connect('mongodb+srv://linkedin-hot-leads:ilXR2tTqnF2XDQlW@linkedin-db.tt3ronc.mongodb.net/?retryWrites=true&w=majority');
+    try {
+        await mongoose.connect('mongodb+srv://linkedin-hot-leads:ilXR2tTqnF2XDQlW@linkedin-db.tt3ronc.mongodb.net/?retryWrites=true&w=majority');
 
-    if (!mongoose.modelNames().includes("Cookie")) {
-        const cookieSchema = new mongoose.Schema({
-            user_id: String,
-            li_at: String,
-            cookie_str: String
-        });
+        if (!mongoose.modelNames().includes("Cookie")) {
+            const cookieSchema = new mongoose.Schema({
+                user_id: String,
+                li_at: String,
+                cookie_str: String
+            });
 
-        mongoose.model('Cookie', cookieSchema);
-    };
-
+            mongoose.model('Cookie', cookieSchema);
+        };
+    } catch (err) {
+        console.trace(err);
+    }
 };
 
 module.exports = main;
